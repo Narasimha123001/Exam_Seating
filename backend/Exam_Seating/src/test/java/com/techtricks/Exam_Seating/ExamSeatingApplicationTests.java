ebac9f4423cf7daa00d1b8909c619241e13cf159
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-22 17:15:42 +0530

    add: added the bolier plate code

---
+++ backend/Exam_Seating/src/test/java/com/techtricks/Exam_Seating/ExamSeatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtricks.Exam_Seating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSeatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
