f8e36d8369ddbb1dc07fbf71000befa3a191d563
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-24 18:37:14 +0530

    add: added the subjects for the dept

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/model/Subject.java
@@ -0,0 +1,47 @@
+package com.techtricks.Exam_Seating.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CollectionId;
+
+import java.util.List;
+
+@Entity
+@Table(name = "subject")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long subjectId;
+
+    @Column(nullable = false , unique = true)
+    private String subjectCode;
+
+    @Column(nullable = false )
+    private String title;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "subject_departments",
+            joinColumns = @JoinColumn(name = "subject_id"),
+            inverseJoinColumns = @JoinColumn(name = "dept_id")
+    )
+    private List<Department> departments;
+
+    private int semester;
+
+//    @Enumerated(EnumType.STRING)
+//    private PaperType paperType;
+
+    private int parts;
+
+//    private int durationMinutes;
+}
