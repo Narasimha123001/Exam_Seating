68637e5e1a4859fa2ae068cfc22ede1274c5037d
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-26 09:33:27 +0530

    "add: added the ScanLog Repository for manage student attendance data"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/ScanLogRepository.java
@@ -0,0 +1,9 @@
+package com.techtricks.Exam_Seating.repository;
+
+import com.techtricks.Exam_Seating.model.ScanLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScanLogRepository extends JpaRepository<ScanLog,Long> {
+
+
+}
