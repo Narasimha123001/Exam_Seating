b35faef0d8ba1c227d92b81bce9b468aaf7061a2
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-26 09:37:07 +0530

    "add: added the Student Repository for manage Student data"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/StudentRepository.java
@@ -0,0 +1,17 @@
+package com.techtricks.Exam_Seating.repository;
+
+import com.techtricks.Exam_Seating.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student , Long> {
+
+    Optional<Student> findByRegisterNo(Long registerNo);
+
+
+}
