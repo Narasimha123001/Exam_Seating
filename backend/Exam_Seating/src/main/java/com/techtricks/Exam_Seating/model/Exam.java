cb7f108003f5b453d17cb3ca2e4d01a937bf017e
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-26 09:06:59 +0530

    remove: removed the unwanted imports

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/model/Exam.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/model/Exam.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalTime;
 
 @Entity
 @Table(name = "exam")
