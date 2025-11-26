9244ed6a4c9c1ffd500d2576964505cfec7c01ad
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-26 09:32:31 +0530

    "add: added the Invigilator Repository for manage Invigilator data"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/InvigilatorRepository.java
@@ -0,0 +1,10 @@
+package com.techtricks.Exam_Seating.repository;
+
+import com.techtricks.Exam_Seating.model.Invigilator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface InvigilatorRepository extends JpaRepository<Invigilator,Long> {
+
+
+}
