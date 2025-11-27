5e6c0fc94bb1eefb088a63e5833a189edf985af0
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-27 22:14:51 +0530

    "add: added the StudentService and impl for manage Student curd operation "

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/services/StudentService.java
@@ -0,0 +1,24 @@
+package com.techtricks.Exam_Seating.services;
+
+import com.techtricks.Exam_Seating.dto.StudentRequest;
+import com.techtricks.Exam_Seating.dto.StudentResponse;
+import com.techtricks.Exam_Seating.dto.SubjectRequest;
+import com.techtricks.Exam_Seating.model.Student;
+import com.techtricks.Exam_Seating.model.Subject;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface StudentService {
+
+   // public Student addStudent(Long regNo, String name, Long deptId, int year, int sem);
+    public Student addStudent(StudentRequest studentRequest);
+
+    public Student getStudent(Long registrationId);
+
+    List<Student> addStudentBulk(List<StudentRequest> list);
+
+
+    public StudentResponse getStudentWithSubjects(Long registerNo);
+}
---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/services/StudentServiceImpl.java
@@ -0,0 +1,118 @@
+package com.techtricks.Exam_Seating.services;
+
+import com.techtricks.Exam_Seating.dto.StudentRequest;
+import com.techtricks.Exam_Seating.dto.StudentResponse;
+import com.techtricks.Exam_Seating.dto.SubjectRequest;
+import com.techtricks.Exam_Seating.dto.SubjectResponse;
+import com.techtricks.Exam_Seating.model.Department;
+import com.techtricks.Exam_Seating.model.Student;
+import com.techtricks.Exam_Seating.model.Subject;
+import com.techtricks.Exam_Seating.repository.DepartmentRepository;
+import com.techtricks.Exam_Seating.repository.StudentRepository;
+import com.techtricks.Exam_Seating.repository.SubjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+    private final DepartmentRepository departmentRepository;
+    private final SubjectRepository subjectRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository, DepartmentRepository departmentRepository, SubjectRepository subjectRepository) {
+        this.studentRepository = studentRepository;
+        this.departmentRepository = departmentRepository;
+        this.subjectRepository = subjectRepository;
+    }
+
+    @Override
+    public Student addStudent(StudentRequest st) {
+
+        Department d = departmentRepository.findById(st.getDepId())
+                .orElseThrow(() -> new RuntimeException("Department not found"));
+
+        Student s = Student.builder()
+                .registerNo(st.getRegistrationId())
+                .name(st.getName())
+                .department(d)
+                .year(st.getYear())
+                .semester(st.getSem())
+                .email(st.getEmail())
+                .phone(st.getPhone())
+
+                .build();
+        return studentRepository.save(s);
+    }
+
+
+
+    @Override
+    public Student getStudent(Long registrationId){
+        Optional<Student> optional = studentRepository.findByRegisterNo(registrationId);
+        return optional.orElse(null);
+    }
+
+    @Override
+    public List<Student> addStudentBulk(List<StudentRequest> list) {
+        List<Student> students = new ArrayList<>();
+
+        for (StudentRequest st : list) {
+            Department d = departmentRepository.findById(st.getDepId())
+                    .orElseThrow(() -> new RuntimeException("Department not found"));
+
+
+            Student s = Student.builder()
+                    .registerNo(st.getRegistrationId())
+                    .name(st.getName())
+                    .department(d)
+                    .year(st.getYear())
+                    .semester(st.getSem())
+                    .email(st.getEmail())
+                    .phone(st.getPhone())
+                    .build();
+
+
+            students.add(s);
+
+        }
+        return studentRepository.saveAll(students);
+    }
+
+    @Override
+    public StudentResponse getStudentWithSubjects(Long registerNo) {
+
+        Student student = studentRepository.findByRegisterNo(registerNo)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+
+        List<Subject> subjectList = subjectRepository.findByDeptSemYear(
+                student.getDepartment().getDeptId(),
+                student.getSemester(),
+                student.getYear()
+        );
+
+        // Convert to DTO
+        List<SubjectResponse> subjectResponses = subjectList.stream().map(s -> {
+            SubjectResponse dto = new SubjectResponse();
+          //  dto.setSubjectId(s.getSubjectId());
+            dto.setSubjectCode(s.getSubjectCode());
+            dto.setTitle(s.getTitle());
+         //   dto.setSemester(s.getSemester());
+         //   dto.setYear(s.getYear());
+            return dto;
+        }).toList();
+
+        StudentResponse res = new StudentResponse();
+        res.setRegistrationId(student.getRegisterNo());
+        res.setName(student.getName());
+        res.setYear(student.getYear());
+        res.setSemester(student.getSemester());
+        res.setSubjects(subjectResponses);
+
+        return res;
+    }
+
+}
