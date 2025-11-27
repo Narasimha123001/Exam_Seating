c626df4090bab211e527f174754db89713feed43
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-11-27 22:16:42 +0530

    "add: added the ExamSession Services and impl for manage exam Session "

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/services/ExamSessionService.java
@@ -0,0 +1,23 @@
+package com.techtricks.Exam_Seating.services;
+
+
+import com.techtricks.Exam_Seating.dto.ExamSessionCreateRequest;
+import com.techtricks.Exam_Seating.dto.ExamSessionResponse;
+import com.techtricks.Exam_Seating.model.ExamSession;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ExamSessionService {
+
+    public ExamSessionResponse create(ExamSessionCreateRequest session);
+
+    public List<ExamSession> findAll();
+
+    public ExamSession getById(Long sessionId);
+
+    public Long calculateCapacity(Long sessionId);
+
+    public ExamSession updateCapacityRequired(Long sessionId);
+}
---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/services/ExamSessionServiceImpl.java
@@ -0,0 +1,112 @@
+package com.techtricks.Exam_Seating.services;
+
+import com.techtricks.Exam_Seating.dto.ExamSessionCreateRequest;
+import com.techtricks.Exam_Seating.dto.ExamSessionResponse;
+import com.techtricks.Exam_Seating.model.Exam;
+import com.techtricks.Exam_Seating.model.ExamSession;
+import com.techtricks.Exam_Seating.model.Subject;
+import com.techtricks.Exam_Seating.repository.ExamRepository;
+import com.techtricks.Exam_Seating.repository.ExamSessionRepository;
+import com.techtricks.Exam_Seating.repository.StudentSessionRepository;
+import com.techtricks.Exam_Seating.repository.SubjectRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ExamSessionServiceImpl implements ExamSessionService {
+
+    private final ExamSessionRepository examSessionRepository;
+    private final StudentSessionRepository studentSessionRepository;
+
+    private final SubjectRepository subjectRepository;
+    private final ExamRepository examRepository;
+
+    public ExamSessionServiceImpl(ExamSessionRepository examSessionRepository, StudentSessionRepository studentSessionRepository, SubjectRepository subjectRepository, ExamRepository examRepository) {
+        this.examSessionRepository = examSessionRepository;
+        this.studentSessionRepository = studentSessionRepository;
+        this.subjectRepository = subjectRepository;
+        this.examRepository = examRepository;
+    }
+
+    @Override
+    @Transactional
+    public ExamSessionResponse  create(ExamSessionCreateRequest dto) {
+
+
+            Exam exam = examRepository .findById(dto.getExamId())
+                    .orElseThrow(() -> new RuntimeException("Exam not found: " + dto.getExamId()));
+
+            Subject subject = subjectRepository.findById(dto.getSubjectId())
+                    .orElseThrow(() -> new RuntimeException("Subject not found: " + dto.getSubjectId()));
+
+            ExamSession session = ExamSession.builder()
+                    .exam(exam)
+                    .subject(subject)
+                    .date(dto.getDate())
+                    .slotCode(dto.getSlotCode())
+                    .startTime(dto.getStartTime())
+
+                    .endTime(dto.getEndTime())
+                    .partNo(dto.getPartNo())
+                    // capacityRequired default = 0 (from entity)
+                    .build();
+          //  System.out.println(session);
+
+            ExamSession saved =  examSessionRepository.save(session);
+
+
+            return mapToResponse(saved);
+    }
+
+
+
+    @Override
+    public List<ExamSession> findAll() {
+        return examSessionRepository.findAll();
+    }
+
+    @Override
+    public ExamSession getById(Long sessionId) {
+        return examSessionRepository.findById(sessionId)
+                .orElseThrow(() -> new RuntimeException("session not found"));
+    }
+
+
+    @Override
+    public Long calculateCapacity(Long sessionId) {
+
+        return studentSessionRepository.countBySession_SessionId(sessionId);
+    }
+
+    @Transactional
+    public ExamSession updateCapacityRequired(Long sessionId) {
+        ExamSession session = getById(sessionId);
+
+        long count = calculateCapacity(sessionId);
+        session.setCapacityRequired((int) count);
+
+        return examSessionRepository.save(session);
+    }
+
+
+    private ExamSessionResponse mapToResponse(ExamSession session) {
+
+        ExamSessionResponse response = new ExamSessionResponse();
+        response.setSessionId(session.getSessionId());
+        response.setName(session.getExam().getName()); // depends on your Exam entity
+        response.setSubject_title(session.getSubject().getTitle());
+        response.setDate(session.getDate());
+        response.setSlotCode(session.getSlotCode());
+        response.setStartTime(session.getStartTime());
+        response.setEndTime(session.getEndTime());
+
+        response.setPartNo(session.getPartNo());
+     //   System.out.println(response);
+
+        return response;
+    }
+
+
+}
