17c4f220a74e4577e3ffb58e48b8289446bc769f
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-04 14:32:24 +0530

    "add: added the Subject Controller  for the subject rest controller"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/controllers/SubjectController.java
@@ -0,0 +1,44 @@
+package com.techtricks.Exam_Seating.controllers;
+
+import com.techtricks.Exam_Seating.dto.SubjectRequest;
+import com.techtricks.Exam_Seating.model.Subject;
+import com.techtricks.Exam_Seating.repository.SubjectRepository;
+import com.techtricks.Exam_Seating.services.SubjectService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subjects")
+public class SubjectController {
+
+    private final  SubjectRepository  subjectRepository;
+    private final SubjectService subjectService;
+
+    public SubjectController(SubjectRepository subjectRepository, SubjectService subjectService) {
+        this.subjectRepository = subjectRepository;
+        this.subjectService = subjectService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Subject> create(@RequestBody Subject sbu){
+        return ResponseEntity.ok(subjectRepository.save(sbu));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Subject>> getAll(){
+        return ResponseEntity.ok(subjectRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Subject> getSubjectById(@PathVariable Long id){
+        return ResponseEntity.of(subjectRepository.findById(id));
+    }
+
+    @PostMapping("/bulk")
+    public ResponseEntity<List<Subject>> addSubjectsBulk(@RequestBody List<SubjectRequest> subjectRequests){
+        List<Subject> created = subjectService.addSubjectsBulk(subjectRequests);
+        return ResponseEntity.ok(created);
+    }
+}
