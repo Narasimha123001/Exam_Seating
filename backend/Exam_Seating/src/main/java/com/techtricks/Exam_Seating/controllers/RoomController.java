5d5c9f3c8f7815cc2df2fa71ba944ac7e3e180c0
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-04 14:23:37 +0530

    "add: added the Room controller  for room operations"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/controllers/RoomController.java
@@ -0,0 +1,49 @@
+package com.techtricks.Exam_Seating.controllers;
+
+import com.techtricks.Exam_Seating.dto.RoomRequest;
+import com.techtricks.Exam_Seating.model.Room;
+import com.techtricks.Exam_Seating.repository.ExamSessionRepository;
+import com.techtricks.Exam_Seating.repository.RoomRepository;
+import com.techtricks.Exam_Seating.repository.StudentSessionRepository;
+import com.techtricks.Exam_Seating.services.RoomService;
+import com.techtricks.Exam_Seating.services.StudentSessionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/room")
+public class RoomController {
+
+    private final RoomService roomService;
+    private final RoomRepository roomRepository;
+    private final ExamSessionRepository examSessionRepository;
+
+    public RoomController(RoomService roomService, RoomRepository roomRepository,  ExamSessionRepository examSessionRepository) {
+        this.roomService = roomService;
+        this.roomRepository = roomRepository;
+        this.examSessionRepository = examSessionRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Room> create(@RequestBody Room room){
+        Room saved = roomRepository.save(room);
+        roomService.generatedSeatsForRoom(saved.getRoomId());
+        return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Room>> getAll(){
+        List<Room> saved = roomRepository.findAll();
+        return ResponseEntity.ok(saved);
+    }
+    @PostMapping("/bulk")
+    public ResponseEntity<List<Room>> addRoomsBulk(@RequestBody List<RoomRequest> requests) {
+        return ResponseEntity.ok(roomService.addRoomsBulk(requests));
+    }
+
+
+}
