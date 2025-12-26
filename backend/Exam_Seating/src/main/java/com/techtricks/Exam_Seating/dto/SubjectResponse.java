07dd29194f11d92e11876f2534df741403f2557c
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-26 18:37:08 +0530

    add: added  the subject respone for user respone

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/SubjectResponse.java
@@ -0,0 +1,12 @@
+package com.techtricks.Exam_Seating.dto;
+
+import lombok.Data;
+
+@Data
+public class SubjectResponse {
+   // private Long subjectId;
+    private String subjectCode;
+    private String title;
+  //  private int semester;
+  //  private int year;
+}
