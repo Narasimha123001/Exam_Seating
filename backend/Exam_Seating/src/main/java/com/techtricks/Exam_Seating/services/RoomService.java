6f50ad017c38ec3037531b50142f443e4a0f511d
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-26 18:00:26 +0530

    update: add the room for the select student repo

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/services/RoomService.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/services/RoomService.java
@@ -4,6 +4,7 @@ package com.techtricks.Exam_Seating.services;
 import com.techtricks.Exam_Seating.dto.RoomRequest;
 import com.techtricks.Exam_Seating.model.Room;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ import java.util.List;
 public interface RoomService {
     public void generatedSeatsForRoom(Long roomId);
     List<Room> addRoomsBulk(List<RoomRequest> list);
+
+    public List<Long> selectRoomsForTotalStudents(int totalStudents);
 }
