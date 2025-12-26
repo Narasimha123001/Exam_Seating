c64db7e548c6df7b528fa35acdf16b3eed8ad6d5
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-26 17:52:44 +0530

    update : updated the all room capacity by decresing oreder wise

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/RoomRepository.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/RoomRepository.java
@@ -2,8 +2,15 @@ package com.techtricks.Exam_Seating.repository;
 
 import com.techtricks.Exam_Seating.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
+    @Query("SELECT r FROM Room r ORDER BY r.totalCapacity DESC")
+
+    List<Room> findAllByOrderByTotalCapacityDesc();
 }
