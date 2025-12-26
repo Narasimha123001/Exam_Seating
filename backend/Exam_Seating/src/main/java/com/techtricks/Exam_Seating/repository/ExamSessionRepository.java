d1f38b1f3f2d3cb360ad08e541bf04d42652496f
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-26 17:51:03 +0530

    update: added the capacity in room and find the session by exam date

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/ExamSessionRepository.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/ExamSessionRepository.java
@@ -2,11 +2,29 @@ package com.techtricks.Exam_Seating.repository;
 
 import com.techtricks.Exam_Seating.model.ExamSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ExamSessionRepository extends JpaRepository<ExamSession,Long> {
+    ExamSession findBySessionId(Long sessionId);
     List<ExamSession> findByExam_ExamId(Long examId);
+
+
+    @Query("SELECT e.capacityRequired FROM ExamSession e WHERE e.sessionId = :sessionId")
+    Integer getCapacityBySessionId(@Param("sessionId") Long sessionId);
+
+
+
+    @Query("SELECT e FROM ExamSession e " +
+            "WHERE e.date = :date AND e.startTime LIKE CONCAT(:time, '%')")
+    List<ExamSession> findByDateAndStartTime(
+            @Param("date") String date,
+            @Param("time") String time);
+
+
 }
+
