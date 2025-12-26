ed5a15689ef1ede7e6bb101a9c19cf54678a8383
Narasimha123001 <narasimhaanguluri001@gmail.com>
2025-12-26 18:13:00 +0530

    add: added the dto for student reqister

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/StudentRegisterRequest.java
@@ -0,0 +1,12 @@
+package com.techtricks.Exam_Seating.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentRegisterRequest {
+
+    private Long sessionId;
+    private Long registrationNo;
+
+}
+
