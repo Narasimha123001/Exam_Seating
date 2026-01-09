594589211176fe0c54c812f522c15454666bc269
Narasimha <narasimhaanguluri001@gmail.com>
2026-01-10 00:36:39 +0530

    update: updated the seatRepo with countbyroomid

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/SeatRepository.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/SeatRepository.java
@@ -9,6 +9,16 @@ import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat,Long> {
 
+    List<Seat> findByRoom_RoomIdInOrderByRoom_RoomIdAscBenchIndexAscPosNoAsc(
+            List<Long> roomIds
+    );
+
     @Query("SELECT s FROM Seat s WHERE s.room.roomId IN :roomIds ORDER BY s.rowNo, s.colNo, s.posNo")
     List<Seat> getSeatsForRooms(@Param("roomIds") List<Long> roomIds);
+
+    /**
+     * Count total seats in rooms
+     */
+    @Query("SELECT COUNT(s) FROM Seat s WHERE s.room.roomId IN :roomIds")
+    long countByRoomIdIn(@Param("roomIds") List<Long> roomIds);
 }
