47f63e05e8b083d4afc3c5a7a1eb8fc40ac7c161
Narasimha <narasimhaanguluri001@gmail.com>
2026-01-12 01:18:19 +0530

    "update:add the total capacity for single room"

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/RoomRequest.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/RoomRequest.java
@@ -1,5 +1,6 @@
 package com.techtricks.Exam_Seating.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -7,5 +8,6 @@ public class RoomRequest {
     private String name;
     private int benchesTotal;
     private int seatsPerBench;
+    private int totalCapacity;
     private String location;
 }
