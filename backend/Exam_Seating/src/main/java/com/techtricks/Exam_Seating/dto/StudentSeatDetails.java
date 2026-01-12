af3e28c5a66f6bdc8a1d0a992d77236a50794520
Narasimha <narasimhaanguluri001@gmail.com>
2026-01-12 18:23:51 +0530

    "add: added the studentSeatDetails Dto"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/StudentSeatDetails.java
@@ -0,0 +1,11 @@
+package com.techtricks.Exam_Seating.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentSeatDetails {
+    private Long registerNo;
+    private Long roomNo;
+//    private String examDate;
+//    private String examName;
+}
