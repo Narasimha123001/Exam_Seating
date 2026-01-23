a1d6bba86382e5fd140e5d90513c8aa533f8f3e3
Narasimha <narasimhaanguluri001@gmail.com>
2026-01-23 23:44:00 +0530

    "update : added the find student by email method for student "

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/StudentRepository.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/repository/StudentRepository.java
@@ -4,7 +4,6 @@ import com.techtricks.Exam_Seating.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 
@@ -14,4 +13,5 @@ public interface StudentRepository extends JpaRepository<Student , Long> {
     Optional<Student> findByRegisterNo(Long registerNo);
 
 
+    Optional<Student> findByEmail(String email);
 }
