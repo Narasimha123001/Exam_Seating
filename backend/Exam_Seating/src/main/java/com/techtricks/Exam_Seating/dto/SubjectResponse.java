cf4d4ab4338597a5554f7a83d434c6539caa234e
Narasimha <narasimhaanguluri001@gmail.com>
2026-01-28 02:26:42 +0530

    "refactor : remove the unwanted fields of the subject response"

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/SubjectResponse.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/SubjectResponse.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 @Data
 public class SubjectResponse {
-   // private Long subjectId;
     private String subjectCode;
     private String title;
-  //  private int semester;
-  //  private int year;
 }
