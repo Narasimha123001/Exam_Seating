cc8c1f70b397ae883047679d81b94740f74bf65c
Narasimha <narasimhaanguluri001@gmail.com>
2026-01-28 02:23:48 +0530

    "refactor: remove the unwanted fields for Student Response"

--- backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/StudentResponse.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/Exam_Seating/dto/StudentResponse.java
@@ -1,16 +1,10 @@
 package com.techtricks.Exam_Seating.dto;
-
-import com.techtricks.Exam_Seating.model.Subject;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class StudentResponse {
-
-    private Long registrationId;
-    private String name;
-    private int year;
-    private int semester;
+    private String email;
     private List<SubjectResponse> subjects;
 }
