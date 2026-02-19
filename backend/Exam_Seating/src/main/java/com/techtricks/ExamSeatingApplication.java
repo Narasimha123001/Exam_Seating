487f3626484824c1da49c400a3aa4f680d7abd9c
Narasimha <narasimhaanguluri001@gmail.com>
2026-02-19 23:51:20 +0530

    "add: added the CoeAuth application Stater"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/ExamSeatingApplication.java
@@ -0,0 +1,14 @@
+package com.techtricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ExamSeatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSeatingApplication.class, args);
+	}
+
+}
