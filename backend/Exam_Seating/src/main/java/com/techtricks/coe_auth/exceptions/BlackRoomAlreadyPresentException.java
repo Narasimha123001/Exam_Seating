f334f19317eee198eb24fbba74f47a6e699f5626
Narasimha <narasimhaanguluri001@gmail.com>
2026-02-23 04:52:40 +0530

    "feat(exception/blackroom): add exception for duplicate room creation"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/exceptions/BlackRoomAlreadyPresentException.java
@@ -0,0 +1,7 @@
+package com.techtricks.coe_auth.exceptions;
+
+public class BlackRoomAlreadyPresentException extends Exception {
+    public BlackRoomAlreadyPresentException(String message) {
+        super(message);
+    }
+}
