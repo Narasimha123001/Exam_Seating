682f91638e4c4a193b8c419412c22eae63ad9241
Narasimha <narasimhaanguluri001@gmail.com>
2026-02-23 04:13:32 +0530

    "feat(auth): add LoginRequest DTO for authentication input"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/dtos/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.techtricks.coe_auth.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
