520d00e0323ca11573d73ea70dcf95545d744efd
Narasimha <narasimhaanguluri001@gmail.com>
2026-02-23 04:13:00 +0530

    "feat(dto): add BlackRoomResponse for room response mapping"

---
+++ backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/dtos/BlackRoomResponse.java
@@ -0,0 +1,17 @@
+package com.techtricks.coe_auth.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BlackRoomResponse {
+
+    private Long blackRoomNumber;
+    private String blackRoomName;
+
+
+
+}
