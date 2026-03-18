c3ed14ed1533c4b5d40bee8913959a0bfff240e5
Narasimha <narasimhaanguluri001@gmail.com>
2026-03-18 19:26:29 +0530

    "rechange : change the paraMeters to dto for confidential the data"

--- backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/services/BlackRoomService.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/services/BlackRoomService.java
@@ -1,20 +1,33 @@
 package com.techtricks.coe_auth.services;
 
+import com.techtricks.coe_auth.dtos.BlackRoomRequest;
 import com.techtricks.coe_auth.dtos.BlackRoomResponse;
+import com.techtricks.coe_auth.dtos.BlackRoomUpdateRequest;
+import com.techtricks.coe_auth.exceptions.BlackRoomAlreadyPresentException;
 import com.techtricks.coe_auth.exceptions.BlackRoomNotFoundExceptions;
 import com.techtricks.coe_auth.models.BlackRoom;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface BlackRoomService {
 
-    public BlackRoomResponse addBlackRoom(String blackRoomName);
+    public BlackRoomResponse addBlackRoom(BlackRoomRequest request) throws BlackRoomAlreadyPresentException;
 
-    public BlackRoomResponse updateBlackRoom(Long blackRoomId , String BlackRoomName) throws BlackRoomNotFoundExceptions;
+    public BlackRoomResponse updateBlackRoom(BlackRoomUpdateRequest request) throws BlackRoomNotFoundExceptions;
 
     public void removeBlackRoom(Long blackRoomId) throws BlackRoomNotFoundExceptions;
 
     public BlackRoom findBlackRoom(Long blackRoomId) throws BlackRoomNotFoundExceptions;
+
+
+    public BlackRoom findBlackRoomByName(String blackRoomName) throws BlackRoomNotFoundExceptions;
+
+
+    public BlackRoom findBlackRoomByNumber(Long blackRoomNumber) throws BlackRoomNotFoundExceptions;
+
+    List<BlackRoomResponse> getAllBlackRooms() throws BlackRoomNotFoundExceptions;
 
 
 }
--- backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/services/BlackRoomServiceImpl.java
+++ backend/Exam_Seating/src/main/java/com/techtricks/coe_auth/services/BlackRoomServiceImpl.java
@@ -1,11 +1,15 @@
 package com.techtricks.coe_auth.services;
 
+import com.techtricks.coe_auth.dtos.BlackRoomRequest;
 import com.techtricks.coe_auth.dtos.BlackRoomResponse;
+import com.techtricks.coe_auth.dtos.BlackRoomUpdateRequest;
+import com.techtricks.coe_auth.exceptions.BlackRoomAlreadyPresentException;
 import com.techtricks.coe_auth.exceptions.BlackRoomNotFoundExceptions;
 import com.techtricks.coe_auth.models.BlackRoom;
 import com.techtricks.coe_auth.repositorys.BlackRoomRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,23 +22,35 @@ public class BlackRoomServiceImpl implements BlackRoomService {
     }
 
     @Override
-    public BlackRoomResponse addBlackRoom(String blackRoomName) {
+    public BlackRoomResponse addBlackRoom(BlackRoomRequest request)
+            throws BlackRoomAlreadyPresentException {
+
+        Optional<BlackRoom> exitingRoom =
+                blackRoomRepository.findByBlackRoomName(request.getBlackRoomName());
+
+        if (exitingRoom.isPresent()) {
+            throw  new BlackRoomAlreadyPresentException("Room already present");
+        }
+
         BlackRoom blackRoom = new BlackRoom();
-        blackRoom.setBlackRoomName(blackRoomName);
+        blackRoom.setBlackRoomNumber(request.getBlackRoomNumber());
+        blackRoom.setBlackRoomName(request.getBlackRoomName());
         BlackRoom savedBlackRoom =  blackRoomRepository.save(blackRoom);
-        return new  BlackRoomResponse(savedBlackRoom.getBlackRoomId() , savedBlackRoom.getBlackRoomName());
+
+        return new  BlackRoomResponse(savedBlackRoom.getBlackRoomNumber(), savedBlackRoom.getBlackRoomName());
     }
 
     @Override
-    public BlackRoomResponse updateBlackRoom(Long blackRoomId , String blackRoomName) throws BlackRoomNotFoundExceptions {
-        Optional<BlackRoom> optionalBlackRoom = blackRoomRepository.findById(blackRoomId);
+    public BlackRoomResponse updateBlackRoom(BlackRoomUpdateRequest request) throws BlackRoomNotFoundExceptions {
+        Optional<BlackRoom> optionalBlackRoom = blackRoomRepository.findByBlackRoomId(request.getBlackRoomId());
         if(optionalBlackRoom.isEmpty()) {
             throw new BlackRoomNotFoundExceptions("Room not found");
         }
         BlackRoom blackRoom = optionalBlackRoom.get();
-        blackRoom.setBlackRoomName(blackRoomName);
+        blackRoom.setBlackRoomName(request.getBlackRoomName());
+        blackRoom.setBlackRoomNumber(request.getBlackRoomNumber());
         BlackRoom savedBlackRoom =  blackRoomRepository.save(blackRoom);
-        return new BlackRoomResponse(savedBlackRoom.getBlackRoomId() , savedBlackRoom.getBlackRoomName());
+        return new BlackRoomResponse(savedBlackRoom.getBlackRoomNumber(), savedBlackRoom.getBlackRoomName());
     }
 
     @Override
@@ -54,6 +70,37 @@ public class BlackRoomServiceImpl implements BlackRoomService {
             throw new BlackRoomNotFoundExceptions("BlackRoom not found");
         }
         return optionalBlackRoom.get();
+    }
+
+    @Override
+    public BlackRoom findBlackRoomByName(String blackRoomName) throws BlackRoomNotFoundExceptions {
+        Optional<BlackRoom> optionalBlackRoom = blackRoomRepository.findByBlackRoomName(blackRoomName);
+        if(optionalBlackRoom.isEmpty()) {
+            throw new BlackRoomNotFoundExceptions("BlackRoom not found");
+        }
+        return optionalBlackRoom.get();
+    }
+
+    @Override
+    public BlackRoom findBlackRoomByNumber(Long blackRoomNUmber) throws BlackRoomNotFoundExceptions {
+        Optional<BlackRoom> optionalBlackRoom = blackRoomRepository.findByBlackRoomNumber(blackRoomNUmber);
+        if(optionalBlackRoom.isEmpty()) {
+            throw new BlackRoomNotFoundExceptions("BlackRoom not found");
+        }
+        return optionalBlackRoom.get();
+    }
+
+    @Override
+    public List<BlackRoomResponse> getAllBlackRooms() throws BlackRoomNotFoundExceptions {
+        List<BlackRoom> blackRooms = blackRoomRepository.findAll();
+        if(blackRooms.isEmpty()) {
+            throw new BlackRoomNotFoundExceptions("BlackRoom not found");
+        }
+        return blackRooms.stream()
+                .map(room -> new BlackRoomResponse(
+                         room.getBlackRoomNumber() , room.getBlackRoomName()
+                ))
+                .toList();
     }
 
 
